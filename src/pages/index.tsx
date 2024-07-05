2b6a36ec88d58ca3150485a9695b601acf8364da
FirstBright <mipo8890@gmail.com>
2024-07-05 20:11:17 +0900

    add modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
+import { useState } from "react"
+import Modal from "react-modal"
 
 const schema = z
     .object({
@@ -31,6 +33,7 @@ const schema = z
             .max(70, { message: "70세 이하만 가입할 수 있어요." }),
         address: z.string().min(1, { message: "주소 필수입니다." }),
         route: z.string().min(1, { message: "경로 필수입니다." }),
+        introduce: z.string().min(1, { message: "본인을 소개해주세요" }),
     })
     .superRefine(({ checkPassword, password }, ctx) => {
         if (checkPassword !== password) {
@@ -41,18 +44,36 @@ const schema = z
             })
         }
     })
+
 type SchemaType = z.infer<typeof schema>
 
 export default function ZodHookFormPage() {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [phoneModalOpen, setPhoneModalOpen] = useState(false)
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [formData, setFormData] = useState<SchemaType | null>(null)
+
     const {
         register,
         handleSubmit,
+        setValue,
         formState: { errors },
     } = useForm<SchemaType>({
         resolver: zodResolver(schema),
     })
+
     const handleSuccess = (data: SchemaType) => {
-        console.log("", data)
+        setFormData(data)
+        console.log(data)
+        setIsModalOpen(true)
+    }
+
+    const handleNumberClick = (number: string) => {
+        setPhoneNumber((prev) => {
+            const newNumber = prev + number
+            setValue("phone", newNumber) // Update the form value
+            return newNumber
+        })
     }
 
     return (
@@ -76,7 +97,7 @@ export default function ZodHookFormPage() {
                         id='email'
                         {...register("email")}
                         placeholder='id@example.com'
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2  text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     />
                     {errors.email?.message && (
                         <span className='text-sm text-red-600'>
@@ -95,7 +116,7 @@ export default function ZodHookFormPage() {
                         id='name'
                         {...register("name")}
                         placeholder='이름을 작성해 주세요'
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     />
                     {errors.name?.message && (
                         <span className='text-sm text-red-600'>
@@ -114,8 +135,9 @@ export default function ZodHookFormPage() {
                         type='password'
                         id='password'
                         {...register("password")}
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     />
+
                     {errors.password?.message && (
                         <span className='text-sm text-red-600'>
                             {errors.password.message.toString()}
@@ -133,7 +155,7 @@ export default function ZodHookFormPage() {
                         type='password'
                         id='passwordCheck'
                         {...register("checkPassword")}
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     />
                     {errors.checkPassword?.message && (
                         <span className='text-sm text-red-600'>
@@ -148,19 +170,71 @@ export default function ZodHookFormPage() {
                     >
                         전화번호
                     </label>
-                    <input
-                        type='text'
-                        id='phone'
-                        {...register("phone")}
-                        placeholder='000-000-0000'
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
-                    />
-                    {errors.phone?.message && (
-                        <span className='text-sm text-red-600'>
-                            {errors.phone.message.toString()}
-                        </span>
-                    )}
+                    <button
+                        type='button'
+                        onClick={() => setPhoneModalOpen(true)}
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                    >
+                        전화번호 입력
+                    </button>
+                    <Modal
+                        isOpen={phoneModalOpen}
+                        contentLabel='Phone Number Modal'
+                        className='w-40 '
+                    >
+                        <div className='keypad grid grid-cols-3 gap-2'>
+                            {Array.from({ length: 10 }, (_, i) => (
+                                <button
+                                    className='h-10 px-5 bg-slate-200'
+                                    key={i === 9 ? "0" : i + 1}
+                                    onClick={() =>
+                                        handleNumberClick(
+                                            i === 9 ? "0" : (i + 1).toString()
+                                        )
+                                    }
+                                >
+                                    {i === 9 ? "0" : i + 1}
+                                </button>
+                            ))}
+                        </div>
+                        <form className='flex flex-col mt-4'>
+                            <input
+                                type='text'
+                                value={phoneNumber}
+                                readOnly
+                                className='block w-full px-4 py-2 mb-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                            />                           
+
+                            <div className='flex justify-between mt-2'>
+                                <button
+                                    type='button'
+                                    onClick={() => {
+                                        setPhoneNumber("")
+                                    }}
+                                    className='px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700'
+                                >
+                                    삭제
+                                </button>
+                                <button
+                                    type='submit'
+                                    {...register("phone")}
+                                    onClick={() => {
+                                        setPhoneModalOpen(false)
+                                    }}
+                                    className='px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700'
+                                >
+                                    확인
+                                </button>
+                            </div>
+                        </form>
+                    </Modal>
+                    {errors.age?.message && (
+                    <span className='text-sm text-red-600'>
+                        {errors.age.message.toString()}
+                    </span>
+                )}
                 </div>
+                
                 <div className='flex flex-col gap-1'>
                     <label
                         htmlFor='age'
@@ -172,13 +246,8 @@ export default function ZodHookFormPage() {
                         type='number'
                         id='age'
                         {...register("age", { valueAsNumber: true })}
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     />
-                    {errors.age?.message && (
-                        <span className='text-sm text-red-600'>
-                            {errors.age.message.toString()}
-                        </span>
-                    )}
                 </div>
                 <div className='flex flex-col gap-1'>
                     <label
@@ -191,7 +260,7 @@ export default function ZodHookFormPage() {
                         type='text'
                         id='address'
                         {...register("address")}
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     />
                     {errors.address?.message && (
                         <span className='text-sm text-red-600'>
@@ -209,7 +278,7 @@ export default function ZodHookFormPage() {
                     <select
                         id='route'
                         {...register("route")}
-                        className='block w-full px-4 py-2 mt-1 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
+                        className='block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300'
                     >
                         <option value=''>선택하세요</option>
                         <option value='AD'>광고</option>
@@ -222,12 +291,30 @@ export default function ZodHookFormPage() {
                         </span>
                     )}
                 </div>
+                <div className='flex flex-col gap-1 h-full'>
+                    <label htmlFor='introduce'>자기소개</label>
+                    <textarea
+                        id='introduce'
+                        rows={10}
+                        style={{ minHeight: "100px" }}
+                        className='block w-full h-5 px-4 py-2 focus:outline-none focus:ring focus:ring-blue-300'
+                        {...register("introduce")}
+                    ></textarea>
+                </div>
                 <button
                     type='submit'
                     className='w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300'
                 >
                     제출
                 </button>
+                <Modal isOpen={isModalOpen} contentLabel='Modal'>
+                    <div className='flex flex-col gap-20'>
+                        <h2>{formData?.name}님 환영합니다.</h2>
+                        <button onClick={() => setIsModalOpen(false)}>
+                            확인
+                        </button>
+                    </div>
+                </Modal>
             </form>
         </div>
     )
